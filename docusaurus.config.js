3ab976f26784b658001a8e34730d6e6f6bd2ad83
AlexandreK <alexandre.kientzel@talan.com>
2024-06-18 13:11:07 +0200

    Update Logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,9 +8,9 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'UnifiedConnect',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/Talan.png',
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-site.example.com',
@@ -66,10 +66,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'UnifiedConnect',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/Talan.png',
         },
         items: [
           {
