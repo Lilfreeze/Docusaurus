9bcc46d839c9f74e155c7cba16c5c861c2a7afb6
AlexandreK <alexandre.kientzel@talan.com>
2024-06-19 13:51:32 +0200

    change github link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Lilfreeze/Docusaurus/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Lilfreeze/Docusaurus/',
         },
         theme: {
           customCss: './src/css/custom.css',
